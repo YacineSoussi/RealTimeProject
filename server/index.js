86b6cac21cbaf05e883318f8084bb87c2c3e1c97
YacineSoussi <Yacine1.soussi@gmail.com>
2022-11-25 17:22:49 +0100

    add back-end logic for users

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 // create server express
 const express = require('express');
+const userRoutes = require('./models/routes/users');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 // create server socket.io
 const server = require('http').createServer(app);
 // const io = require('socket.io')(server);
+
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
+app.use('/users', userRoutes);
+
 
 // create server http
 server.listen(port, () => {
