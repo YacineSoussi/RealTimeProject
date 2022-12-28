4addded4b93b9ee8279643eb1dccc70983906cd7
Pereira Júlio <julioocesarr@outlook.fr>
2022-12-28 23:43:56 +0100

    Indentation + refactoring + add routing for chatbot

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,43 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      // component: Home
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/auth/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/auth/RegisterView.vue')
-    },
-    {
-      path: '/conversation',
-      name: 'conversation',
-      component: () => import('../views/tchats/GlobalView.vue'),
-
-    },
-  ]
-
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: () => import("../views/HomeView.vue"),
+		},
+		{
+			path: "/login",
+			name: "login",
+			component: () => import("../views/auth/LoginView.vue"),
+		},
+		{
+			path: "/register",
+			name: "register",
+			component: () => import("../views/auth/RegisterView.vue"),
+		},
+		{
+			path: "/conversation",
+			name: "conversation",
+			component: () => import("../views/tchats/GlobalView.vue"),
+		},
+		{
+			path: "/chatbot",
+			name: "chatbot",
+			component: () => import("../views/chatbot/ChatbotView.vue"),
+		},
+	],
+});
 
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token')
-  if (to.matched.some(record => record.meta.requiresAuth) && !token) {
-    next({ name: 'login' })
-  } else {
-    next()
-  }
-})
+	const token = localStorage.getItem("token");
+	if (to.matched.some((record) => record.meta.requiresAuth) && !token) {
+		next({ name: "login" });
+	} else {
+		next();
+	}
+});
 
-
-
-export default router
+export default router;
