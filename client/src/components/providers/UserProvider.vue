16b6df55e3308266b36f02022d668bce0aee35da
Pereira Júlio <julioocesarr@outlook.fr>
2023-01-28 01:22:33 +0100

    Fix condition

--- client/src/components/providers/UserProvider.vue
+++ client/src/components/providers/UserProvider.vue
@@ -1,58 +1,54 @@
 <!-- Gestion des données de l'application -->
 <template>
-    <slot :logout="logout" :isAuth="isAuth"></slot>
+	<slot :logout="logout" :isAuth="isAuth"></slot>
 </template>
 
 <script setup>
-import { ref, reactive, provide, watchEffect } from 'vue'
-import AuthLogic from '../../logics/AuthLogic';
-import router from '../../router';
-import UserLogic from '../../logics/UserLogic';
-import LocalStorage from '../../services/LocalStorage';
+import { ref, reactive, provide, watchEffect } from "vue";
+import AuthLogic from "../../logics/AuthLogic";
+import router from "../../router";
+import UserLogic from "../../logics/UserLogic";
+import LocalStorage from "../../services/LocalStorage";
 
-const User = LocalStorage.get('user');
+const User = LocalStorage.get("user");
 const users = ref([]);
 
+const login = (form) => {
+	return AuthLogic.login({ ...form }).then((data) => {
+		isAuth.value = !isAuth.value;
+		user.value = data;
+	});
+};
 
-const login =  (form) => {
-     return AuthLogic.login({...form})
-        .then((data) => {
-                isAuth.value = !isAuth.value; 
-                user.value = data;
-        })
-    };
-
-const  logout = () => {
-    AuthLogic.logout();
-    isAuth.value = !isAuth.value;
-    router.push({ name: 'home' });
-    };
+const logout = () => {
+	AuthLogic.logout();
+	isAuth.value = !isAuth.value;
+	router.push({ name: "home" });
+};
 
 const register = (form) => {
-    return AuthLogic.register({...form});
-    };
- const updateUser = (id, body) => {
-    return UserLogic.updateUser(id, body)
-        .then((data) => {
-            User.status = data.status;
-            return data;
-        });
+	return AuthLogic.register({ ...form });
+};
+const updateUser = (id, body) => {
+	return UserLogic.updateUser(id, body).then((data) => {
+		User.status = data.status;
+		return data;
+	});
 };
 
 let isAuth = ref(AuthLogic.isAuth());
 const user = reactive({});
 
-watchEffect( async() => {
-    users.value = await UserLogic.getUsers();
+watchEffect(async () => {
+	if (isAuth.value) {
+		users.value = await UserLogic.getUsers();
+	}
 });
 
-    provide('Providerlogout', logout);
-    provide('ProviderisAuth', isAuth);
-    provide('ProviderLogin', login);
-    provide('ProviderRegister', register);
-    provide('ProviderUpdateUser', updateUser);
-    provide('ProviderUsers', users);
-
-
-
+provide("Providerlogout", logout);
+provide("ProviderisAuth", isAuth);
+provide("ProviderLogin", login);
+provide("ProviderRegister", register);
+provide("ProviderUpdateUser", updateUser);
+provide("ProviderUsers", users);
 </script>
