dbb8b7bf855000cde07274180ba2f2ab449d910a
Pereira Júlio <julioocesarr@outlook.fr>
2023-01-30 19:21:13 +0100

    Refactoring file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// create server express
 const express = require("express");
 const userRoutes = require("./models/routes/users");
 const securityRoutes = require("./models/routes/security");
@@ -9,9 +8,7 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 const socketIo = require("socket.io");
-
 const server = require("http").createServer(app);
-
 const io = socketIo(server, {
 	cors: {
 		origin: "*",
@@ -31,20 +28,17 @@ app.use("/", messageRoutes);
 app.use("/", participantRoutes);
 app.use("/", conversationRoutes);
 
-// create server http
-server.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
 
-// we register a middleware which checks the username and allows the connection:
+// We register a middleware which checks the username and allows the connection:
 io.use((socket, next) => {
 	const userId = socket.handshake.auth.userId;
-	console.log(userId);
+
 	if (!userId) {
-		return next(new Error("invalid userId"));
+		return next(new Error("Invalid user id"));
 	}
 
-	// On enregistre l'utilisateur dans la socket avec son id donné par le client
+	// We register the user in the socket with his id given by the client
 	socket.userId = userId;
 	socket.join(userId);
 
@@ -52,8 +46,7 @@ io.use((socket, next) => {
 });
 
 io.on("connection", (socket) => {
-	console.log("connection", socket.id);
-	// we send all existing users to the client:
+	// We send all existing users to the client:
 	const users = [];
 
 	// We are looping over the io.of("/").sockets object, which is a Map of all currently connected Socket instances, indexed by ID.
@@ -63,12 +56,11 @@ io.on("connection", (socket) => {
 			userId: socket.userId,
 		});
 	}
+
 	socket.emit("users", users);
 
-	//   We also register a catch-all listener, which is very useful during development:
-	socket.onAny((event, ...args) => {
-		console.log(event, args);
-	});
+	// We also register a catch-all listener, which is very useful during development:
+	socket.onAny((event, ...args) => console.log(event, args));
 
 	socket.on(
 		"message:private",
@@ -101,7 +93,5 @@ io.on("connection", (socket) => {
 		io.to(roomId).emit("join:room", { roomId, userId });
 	});
 
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-	});
+	socket.on("disconnect", () => console.log("user disconnected"));
 });
