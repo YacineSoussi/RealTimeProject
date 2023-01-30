a2a86859cb306187131a0b480017e7d18c9dc648
Pereira Júlio <julioocesarr@outlook.fr>
2023-01-30 19:47:55 +0100

    Refactor file

--- client/src/logics/ConversationLogic.js
+++ client/src/logics/ConversationLogic.js
@@ -1,75 +1,94 @@
-import ConversationRepository from '../repositories/ConversationRepository';
+import ConversationRepository from "../repositories/ConversationRepository";
 
 export default class ConversationLogic {
+	static async getConversations() {
+		const result = await ConversationRepository.getConversations();
 
-    static async getConversations() {
-        const result = await ConversationRepository.getConversations();
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async getConversationOfUser(id) {
-        const result = await ConversationRepository.getConversationOfUser(id);
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async getConversation(id) {
-        const result = await ConversationRepository.getConversation(id);
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data?.message);
+		}
 
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async createConversation(body) {
-        const result = await ConversationRepository.createConversation(body);
+		return result.response.data;
+	}
 
-        if (result.response.status !== 201) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async updateConversation(id, body) {
-        const result = await ConversationRepository.updateConversation(id, body);
+	static async getConversationOfUser(id) {
+		const result = await ConversationRepository.getConversationOfUser(id);
 
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async deleteConversation(id) {
-        const result = await ConversationRepository.deleteConversation(id);
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data?.message);
+		}
 
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async postParticipant({conversationId, userId}) {
-        const result = await ConversationRepository.postParticipant({conversationId, userId});
+		return result.response.data;
+	}
 
-        if (result.response.status !== 201) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async createRoom(body) {
-        const result = await ConversationRepository.createRoom(body);
+	static async getConversation(id) {
+		const result = await ConversationRepository.getConversation(id);
 
-        if (result.response.status !== 201) {
-            throw new Error(result.response.data?.message);
-        }
-        return result.response.data;
-    }
-    static async getParticipants(id) {
-        const result = await ConversationRepository.getParticipants(id);
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data?.message);
+		}
 
-        if (result.response.status !== 200) {
-            throw new Error(result.response.data);
-        }
-        return result.response.data;
-    }
+		return result.response.data;
+	}
+
+	static async createConversation(body) {
+		const result = await ConversationRepository.createConversation(body);
+
+		if (result.response.status !== 201) {
+			throw new Error(result.response.data?.message);
+		}
+
+		return result.response.data;
+	}
+
+	static async updateConversation(id, body) {
+		const result = await ConversationRepository.updateConversation(id, body);
+
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data?.message);
+		}
+		return result.response.data;
+	}
+
+	static async deleteConversation(id) {
+		const result = await ConversationRepository.deleteConversation(id);
+
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data?.message);
+		}
+		return result.response.data;
+	}
+
+	static async postParticipant({ conversationId, userId }) {
+		const result = await ConversationRepository.postParticipant({
+			conversationId,
+			userId,
+		});
+
+		if (result.response.status !== 201) {
+			throw new Error(result.response.data?.message);
+		}
+
+		return result.response.data;
+	}
+
+	static async createRoom(body) {
+		const result = await ConversationRepository.createRoom(body);
+
+		if (result.response.status !== 201) {
+			throw new Error(result.response.data?.message);
+		}
+
+		return result.response.data;
+	}
+
+	static async getParticipants(id) {
+		const result = await ConversationRepository.getParticipants(id);
+
+		if (result.response.status !== 200) {
+			throw new Error(result.response.data);
+		}
+
+		return result.response.data;
+	}
 }
