d40d5df324d4cf86a8bcfbd1b4e03c7cd71e68c2
Pereira Júlio <julioocesarr@outlook.fr>
2023-01-30 19:36:52 +0100

    Refactor files

--- client/src/views/tchats/GlobalView.vue
+++ client/src/views/tchats/GlobalView.vue
@@ -1,30 +1,21 @@
 <script setup>
-import { onMounted } from 'vue';
-import NavConversationsVue from './ListConversations/NavConversationsView.vue';
-import MessagesView from './BlockMessage/MessagesView.vue';
-import ConversationProvider from '../../components/providers/ConversationProvider.vue';
-
-
+import NavConversationsVue from "./ListConversations/NavConversationsView.vue";
+import MessagesView from "./BlockMessage/MessagesView.vue";
+import ConversationProvider from "../../components/providers/ConversationProvider.vue";
 </script>
 
 <template>
-
-<div>
-        <div class="w-full h-32" ></div>
-
-        <div class="container mx-auto" style="margin-top: -128px;">
-            <div class="py-6 h-screen">
-                <div class="flex border border-grey rounded shadow-lg h-full">
-                    <ConversationProvider>
-                    <!-- Left -->
-                    <NavConversationsVue />
-
-                    <!-- Right -->
-                   <MessagesView />
-                    </ConversationProvider>
-                </div>
-            </div>
-        </div>
-    </div>
-
+	<div>
+		<div class="w-full h-32"></div>
+		<div class="container mx-auto" style="margin-top: -128px">
+			<div class="py-6 h-screen">
+				<div class="flex border border-grey rounded shadow-lg h-full">
+					<ConversationProvider>
+						<NavConversationsVue />
+						<MessagesView />
+					</ConversationProvider>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
